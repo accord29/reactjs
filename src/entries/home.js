dad5ed97fe50afecb204edd907b23428363ebb73
Christyan Duarte <christyanduarte@Christyans-MacBook-Pro-15.local>
2019-03-13 16:37:30 -0600

    Manejo de errores

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 //import Playlist from './src/playlist/components/playlist';
-//import data from './src/api.json'
+import data from '../api.json';
 
-const app = document.getElementById('app')
+const homeContainer = document.getElementById('home-container')
 
-const holaMundo = <h1>Hola Estudiante!</h1>;
-
-render(<Home />, app);
+render(<Home data={data} />, homeContainer);
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import Media from './media.js';
 import './playlist.css'
-import FullScreen from '../../icons/components/full-screen';
 
 function Playlist(props) {
-  const playlist = props.data.categories[0].playlist
   return (
     <div className="Playlist">
-      <FullScreen
-        size={50}
-        color="red"
-      />
       {
-        playlist.map((item) => {
-          return <Media {...item} key={item.id} />
+        props.playlist.map((item) => {
+          return <Media handleClick={props.handleOpenModal} {...item} key={item.id} />
         })
       }
     </div>
