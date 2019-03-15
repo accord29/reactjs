6e65532b3697d7217df3baf854987cc0cba75949
Christyan Duarte <christyanduarte@Christyans-MacBook-Pro-15.local>
2019-03-15 16:05:27 -0600

    Agregando lógica al botón de Play y Pausa

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -5,6 +5,7 @@ import Related from '../components/related';
 import ModalContainer from '../../widgets/containers/modal';
 import Modal from '../../widgets/components/modal';
 import HandleError from '../../error/containers/handle-error';
+import VideoPlayer from '../../player/containers/video-player';
 
 class Home extends Component {
   state = {
@@ -27,6 +28,9 @@ class Home extends Component {
       <HandleError>
         <HomeLayout>
           <Related />
+          <VideoPlayer
+            autoplay
+          />
           <Categories
             categories={this.props.data.categories}
             handleOpenModal={this.handleOpenModal}
